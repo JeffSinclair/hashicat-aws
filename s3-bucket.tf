0881c6f879e726473f57ab5b950d3f23051f23b9
Jeff Sinclair <jeff.sinclair@bhp.com>
2022-02-21 05:22:50 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source = "jeffsinclair-testing/s3-bucket/aws"
 
   bucket = "my-s3-bucket"
   bucket_prefix = "jeffsinclair"
